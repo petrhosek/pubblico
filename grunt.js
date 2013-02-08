1306a9e292660a29c02087b2cf7e8938a7e2e526
Petr Hosek <petrhosek@gmail.com>
2013-02-08 22:15:10 +0000

    Generate mocks from apiary.apib

--- grunt.js
+++ grunt.js
@@ -23,6 +23,6 @@ module.exports = function (grunt) {
     var content = grunt.file.read('apiary.apib');
     var blueprint = parser.parse(content);
     var json = JSON.stringify(blueprint.sections, null, 2);
-    grunt.file.write('test/frontend/apiary.js', "var apiary = " + json);
+    grunt.file.write('test/e2e/mocks.js', "var mocks = " + json);
   });
 };
