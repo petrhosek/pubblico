0d5afc742e389022aa1936907a0442e3f8c8bc63
Petr Hosek <petrhosek@gmail.com>
2013-02-09 22:43:24 +0000

    Working on the conference listing

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -31,6 +31,15 @@ angular.module('pubblicoApp').controller('ConferenceDetailCtrl', ['$scope', '$ro
     function ($scope, $routeParams, Conference, Submission) {
   $scope.conference = Conference.get({conference: $routeParams.id});
   $scope.submissions = Submission.query({conference: $routeParams.id});
+  /*$scope.showSubmission = function(id) {
+    $scope.$parent.$broadcast('show', id);
+  };
+  $scope.edit = function(submission) {
+    $scope.submission = submission;
+  };
+  $scope.$on(edit, function() {
+    $scope.$broadcast('show', $scope.submission);
+  });*/
 }]);
 
 angular.module('pubblicoApp').controller('SubmissionsListCtrl', ['$scope', 'Submission',
@@ -48,15 +57,20 @@ angular.module('pubblicoApp').controller('SubmissionsNewCtrl', ['$scope', '$loca
 
 angular.module('pubblicoApp').controller('SubmissionsReadCtrl', ['$scope', '$routeParams', 'Submission',
     function ($scope, $routeParams, Submission) {
-  $scope.submissions = Submission.get({submission: $routeParams.submission})
+  $scope.submission = Submission.get({conference: $routeParams.conference, submission: $routeParams.submission});
 }]);
 
 angular.module('pubblicoApp').controller('SubmissionsEditCtrl', ['$scope', '$location', '$routeParams', 'Submission',
     function ($scope, $location, $routeParams, Submission) {
-  $scope.submission = Submission.get({submission: $routeParams.submission})
+  /*$scope.$on('show', function($_scope, id) {
+    $scope.modalShown = true;
+  });*/
+  $scope.submission = Submission.get({conference: $routeParams.conference, submission: $routeParams.submission});
   $scope.edit = function() {
     $scope.submission.$save();
-  }
+  };
+  /*$scope.close = function() {
+  };*/
 }]);
 
 angular.module('pubblicoApp').controller('SubmissionsDeleteCtrl', ['$scope', '$location', '$routeParams', 'Submission',
