f616ce19a53c15f1e8c2af7d253c86ea62a7620e
Nicholas Ng <nickng@projectfate.org>
2013-02-10 17:15:04 +0000

    Updated submissions/all API

--- routes/api.js
+++ routes/api.js
@@ -3,48 +3,102 @@ var http = require('http');
 var User = require('../models/User.js');
 var Conference = require('../models/Conference.js');
 
+
 /*
  * GET /api/v1/conferences
- * List of (public) conferences.
  */
 exports.conferences = (function (req, res) {
-  Conference.find({}, 'id shortName longName dates', function (err, confs) {
+  Conference.find({}, 'id shortName longName dates description', function (err, confs) {
     if (err) {
       console.log(err);
       res.send(500, {error:'Unable to load list of conferences'});
     } else {
       console.log(confs);
-      // It doesn't make sense to send null when
-      // no public conferences is completely valid
-      res.json((confs !== null) ? confs : []);
+      res.json(confs);
     }
   });
 });
 
 
+/*
+ * POST /api/v1/conferences
+ */
+exports.conference_new = (function (req, res) {
+  console.log(req.body);
+  User.findOne({ email:req.body.user }, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.send(500, {error:'Unable to create conference'})
+    } else {
+      console.log(user);
+      var conf = new Conference({shortName: req.body.shortName, steering: [ user._id ]});
+      console.log(conf);
+      conf.save(function (err) {
+        if (err) {
+          console.log(err);
+          res.send(500, {error:'Cannot save conference'});
+        }
+      });
+      res.send(201);
+    }
+  });
+});
+
 
 /*
  * GET /api/v1/conferences/:conference
- * Metadata of the specified conference.
  */
 exports.conference = (function (req, res) {
-    console.log('conferences conference='+req.params.conference);
-    Conference.findOne({'shortName': req.params.conference}, 'shortName', function (err, conf_metadata) {
-        if (err) {
-            console.log(err);
-            res.send(500, {error:'Unable to load conference'});
-        } else {
-            if (conf_metadata !== null) {
-                console.log(conf_metadata);
-                res.json(conf_metadata);
-            } else {
-                console.log(err);
-                res.send(404, {error:'No matching conference'});
-            }
-        }
-    });
+  console.log('conferences conference='+req.params.conference);
+  Conference.findOne({'shortName': req.params.conference}, 'id shortName longName dates description', function (err, conf_metadata) {
+    if (err) {
+      console.log(err);
+      res.send(500, {error:'Unable to load conference'});
+    } else {
+      if (conf_metadata !== null) {
+        console.log(conf_metadata);
+        res.json(conf_metadata);
+      } else {
+        console.log(err);
+        res.send(404, {error:'No matching conference'});
+      }
+    }
+  });
 });
 
+/*
+ * GET /api/v1/conferenes/:conference/submissions
+ * GET /api/v1/conferences/:conference/submissions/all
+ */
+exports.submissions_all = (function(req, res) {
+  console.log('submissions_all conference='+req.params.conference);
+
+  Conference.findOne({ "shortName":req.params.conference },'submissions', function (err, conf) {
+      console.log(conf);
+      if (err) {
+        console.log(err);
+        res.send(500, {error:'Unable to load submissions'});
+      } else {
+        console.log(conf);
+        for (var i=0; i<conf.submissions.length; i++) {
+          if (conf.submissions[i].authors != undefined) {
+            for (var j=0; j<conf.submissions[i].authors.length; j++) {
+              conf.submissions.current_id = i;
+              User.findById(conf.submissions[i].authors[j], {affiliation:1, email:1, name:1, _id:0}, function (err, u) {
+                if (err) {
+                  console.log(err);
+                  res.send(500);
+                } else {
+                  conf.submissions[conf.submissions.current_id].authors = [ u ];
+                  res.json(conf.submissions);  // XXX This only works for the first author in the first submission..
+                }
+              });
+            }
+          }
+        }
+      }
+    });
+});
 
 /*
  * POST /api/v1/conference/:conference/submissions/user
@@ -71,48 +125,6 @@ exports.submission_new = (function (req, res) {
           res.send(201);
         }
       })
-    }
-  });
-
-});
-
-
-/*
- * GET /api/v1/conferences/:conference/submissions/all
- * List of submissions associated to the specified conference.
- */
-exports.submissions_all = (function(req, res) {
-  console.log('submissions_all conference='+req.params.conference);
-
-  Conference.findOne({ "shortName":req.params.conference },'submissions', function (err, conf) {
-      console.log(conf);
-      if (err) {
-        console.log(err);
-        res.send(500, {error:'Unable to load submissions'});
-      } else {
-        console.log(conf);
-        res.json(conf.submissions);
-      }
-    });
-});
-
-
-/**
- * POST /api/v1/conferences
- */
-exports.createConference = (function (req, res) {
-  console.log(req.body);
-  User.findOne({ email:req.body.user }, function (err, user) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(user);
-      var conf = new Conference({shortName: req.body.shortName, steering: [ user._id ]});
-      console.log(conf);
-      conf.save(function (err) {
-        if (err) console.log(err);
-      });
-      res.send(201);
     }
   });
 });
